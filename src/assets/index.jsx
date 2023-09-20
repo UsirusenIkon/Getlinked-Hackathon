f64992244a9551d0586145f592e045b67bdd6701
Usirusen Ikon <lexicon048@gmail.com>
2023-09-20 15:05:05 +0100

    Reduce assets image size

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -7,14 +7,17 @@ export const purpleIcon = `${purple}`;
 import curvedLine from "./curvedLine.svg"
 export const curvedIcon = `${curvedLine}#img`;
 
-import spark from "./spark.svg"
+import spark from "./spark.png"
 export const sparkIcon = `${spark}#img`;
 
-import chain from "./chain.svg"
+import chain from "./chain.png"
 export const chainIcon = `${chain}#img`;
 
-import guy from "./guy.svg"
+import guy from "./guy.png"
 export const guyIcon = `${guy}#img`;
 
-import bulb from "./bulb.svg"
+import bulb from "./bulb.png"
 export const bulbIcon = `${bulb}#img`;
+
+import idea from "./idea.png"
+export const ideaIcon = `${idea}#img`;
